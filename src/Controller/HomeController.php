611dbe0a85801dc675d325b480559bc48e57f8e2
GabrielLandryPR <landrygabriel960@gmail.com>
2024-04-27 16:09:24 +0200

    reset password

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-//#[Route('/sorir', name: 'app_sortir')]
+#[Route('/sortir', name: 'app_sortir')]
 class HomeController extends AbstractController
 {
 
--- src/Security/ConnectionAuthenticator.php
+++ src/Security/ConnectionAuthenticator.php
@@ -44,13 +44,8 @@ class ConnectionAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Redirection vers la page d'accueil après la connexion réussie
+        return new RedirectResponse($this->urlGenerator->generate('app_sortir_accueil'));
     }
 
     protected function getLoginUrl(Request $request): string
