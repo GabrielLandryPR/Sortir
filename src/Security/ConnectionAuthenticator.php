3229a58e6694a8665a744deff0e775db06bf4cd4
glandry2023 <landrygabriel960@gmail.com>
2024-05-16 12:23:42 +0200

    fix erreurs après rebase

--- src/Security/ConnectionAuthenticator.php
+++ src/Security/ConnectionAuthenticator.php
@@ -6,11 +6,9 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -28,7 +26,6 @@ class ConnectionAuthenticator extends AbstractLoginFormAuthenticator
 
     public function __construct(private UrlGeneratorInterface $urlGenerator, private UserRepository $userRepository)
     {
-
     }
 
     public function authenticate(Request $request): Passport
@@ -60,14 +57,5 @@ class ConnectionAuthenticator extends AbstractLoginFormAuthenticator
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        if ($exception instanceof UserNotFoundException) {
-            $request->getSession()->getFlashBag()->add('error', $exception->getMessage());
-        }
-
-        return new RedirectResponse($this->urlGenerator->generate(self::LOGIN_ROUTE));
     }
 }
